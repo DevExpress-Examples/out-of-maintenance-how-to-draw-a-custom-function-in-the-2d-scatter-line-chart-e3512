f356ac9afcf5b3ee952ef33c8388d629a1710d89
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ScatterLineChartWithCustomFunction/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Xpf.Charts;
+
+namespace ScatterLineChartWithCustomFunction {
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            CreateArchimedianSpiralPoints();
+        }
+
+        void CreateArchimedianSpiralPoints() {
+            for (int i = 0; i < 720; i += 15) {
+                double t = (double)i / 180 * Math.PI;
+                double x = t * Math.Cos(t);
+                double y = t * Math.Sin(t);
+                ArchimedianSpiral.Points.Add(new SeriesPoint(x, y));
+            }
+        }
+    }
+}
---
+++ VB/ScatterLineChartWithCustomFunction/MainWindow.xaml.vb
@@ -0,0 +1,24 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports DevExpress.Xpf.Charts
+
+Namespace ScatterLineChartWithCustomFunction
+
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			CreateArchimedianSpiralPoints()
+		End Sub
+
+		Private Sub CreateArchimedianSpiralPoints()
+			For i As Integer = 0 To 719 Step 15
+				Dim t As Double = CDbl(i) / 180 * Math.PI
+				Dim x As Double = t * Math.Cos(t)
+				Dim y As Double = t * Math.Sin(t)
+				ArchimedianSpiral.Points.Add(New SeriesPoint(x, y))
+			Next i
+		End Sub
+	End Class
+End Namespace
