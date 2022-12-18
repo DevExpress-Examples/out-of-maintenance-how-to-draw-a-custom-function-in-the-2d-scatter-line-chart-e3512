742faac0a0c44cf7c78f59a0964ae83470246580
DevExpressExampleBot <fakeEmail>
2022-12-18 18:34:58 +0400

    Source auto update [skip ci]

---
+++ VB/ScatterLineChartWithCustomFunction/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace ScatterLineChartWithCustomFunction
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/ScatterLineChartWithCustomFunction/MainWindow.xaml.vb
+++ VB/ScatterLineChartWithCustomFunction/MainWindow.xaml.vb
@@ -1,24 +1,24 @@
-﻿Imports System
+Imports System
 Imports System.Windows
 Imports DevExpress.Xpf.Charts
 
 Namespace ScatterLineChartWithCustomFunction
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			CreateArchimedianSpiralPoints()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+            CreateArchimedianSpiralPoints()
+        End Sub
 
-		Private Sub CreateArchimedianSpiralPoints()
-			For i As Integer = 0 To 719 Step 15
-				Dim t As Double = CDbl(i) / 180 * Math.PI
-				Dim x As Double = t * Math.Cos(t)
-				Dim y As Double = t * Math.Sin(t)
-				ArchimedianSpiral.Points.Add(New SeriesPoint(x, y))
-			Next i
-		End Sub
-	End Class
+        Private Sub CreateArchimedianSpiralPoints()
+            For i As Integer = 0 To 720 - 1 Step 15
+                Dim t As Double = CDbl(i) / 180 * Math.PI
+                Dim x As Double = t * Math.Cos(t)
+                Dim y As Double = t * Math.Sin(t)
+                Me.ArchimedianSpiral.Points.Add(New SeriesPoint(x, y))
+            Next
+        End Sub
+    End Class
 End Namespace
